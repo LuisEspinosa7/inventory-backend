db5ccb923939055f934793396d1ccc16d7bdadd2
Luis Espinosa <lluisllanoss@gmail.com>
2022-04-22 18:13:23 -0500

    Category Controller tests completed

--- src/test/java/com/lsoftware/inventory/category/CategoryControllerTest.java
+++ src/test/java/com/lsoftware/inventory/category/CategoryControllerTest.java
@@ -7,8 +7,10 @@ package com.lsoftware.inventory.category;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
@@ -26,6 +28,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lsoftware.inventory.shared.request.RequestPaginationAndSortDTO;
 import com.lsoftware.inventory.shared.response.ResponsePaginationAndSortDTO;
@@ -52,6 +55,67 @@ class CategoryControllerTest {
 	/** The object mapper. */
 	@Autowired
 	private ObjectMapper objectMapper;
+	
+	
+	@Test
+	@WithMockUser(username = "luis3", password = "123456", roles = "ADMIN")
+	void itShouldCreateNewCategory() throws JsonProcessingException, Exception {
+		CategoryDTO request = new CategoryDTO(0L, "Category1", 0);
+		CategoryDTO dto = new CategoryDTO(1L, "CATEGORY1", Status.ACTIVE.getDigit());
+		
+		Mockito.when(categoryService.add(any())).thenReturn(dto);
+		
+		MvcResult result = mockMvc.perform(post("/api/v1/categories/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)))
+				.andDo(MockMvcResultHandlers.print())
+	        	.andExpect(status().isOk())
+	        	.andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Category created"))
+	        	.andExpect(MockMvcResultMatchers.jsonPath("$.data").value(dto))
+	        	.andReturn();
+			
+		String response = result.getResponse().getContentAsString();
+		assertThat(response).isNotNull();	
+	}
+	
+	
+	@Test
+	@WithMockUser(username = "luis3", password = "123456", roles = "ADMIN")
+	void itShouldUpdateCategory() throws JsonProcessingException, Exception {
+		CategoryDTO request = new CategoryDTO(1L, "CATEGORYMOD", Status.ACTIVE.getDigit());
+		CategoryDTO dto = new CategoryDTO(1L, "CATEGORYMOD", Status.ACTIVE.getDigit());
+		
+		Mockito.when(categoryService.update(any())).thenReturn(dto);
+		
+		MvcResult result = mockMvc.perform(put("/api/v1/categories/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)))
+				.andDo(MockMvcResultHandlers.print())
+	        	.andExpect(status().isOk())
+	        	.andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Category updated"))
+	        	.andExpect(MockMvcResultMatchers.jsonPath("$.data").value(dto))
+	        	.andReturn();
+			
+		String response = result.getResponse().getContentAsString();
+		assertThat(response).isNotNull();	
+	}
+	
+	
+	@Test
+	@WithMockUser(username = "luis3", password = "123456", roles = "ADMIN")
+	void itShouldDeleteCategory() throws JsonProcessingException, Exception {
+		
+		MvcResult result = mockMvc.perform(delete("/api/v1/categories/" + 1L )
+				.contentType(MediaType.APPLICATION_JSON))
+				.andDo(MockMvcResultHandlers.print())
+	        	.andExpect(status().isOk())
+	        	.andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Category deleted"))
+	        	.andReturn();
+			
+		String response = result.getResponse().getContentAsString();
+		assertThat(response).isNotNull();	
+	}
+	
 
 	/**
 	 * It should list available categories.

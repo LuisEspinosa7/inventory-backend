e24bad1b9c557b5620b5213d5d261b2a15989ed0
Luis Espinosa <lluisllanoss@gmail.com>
2022-05-04 11:27:01 -0500

    CORS config added

---
+++ src/main/java/com/lsoftware/inventory/cors/RestConfig.java
@@ -0,0 +1,43 @@
+/*
+ * Developed by: Luis Espinosa, be aware that this project
+ * is part of my personal portfolio.
+ */
+package com.lsoftware.inventory.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * The Class RestConfig.
+ * 
+ * @author Luis Espinosa
+ */
+@Configuration
+public class RestConfig {
+	
+	/**
+	 * Cors filter.
+	 *
+	 * @return the cors filter
+	 */
+	@Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addExposedHeader("Authorization");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
